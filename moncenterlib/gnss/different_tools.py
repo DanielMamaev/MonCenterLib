3c1c3229412c19bf0ee5c4032f8e6d676353365e
DanielMamaev <mamaev.s.d@gmail.com>
2023-11-05 15:53:04 +0700

    rename module, add tool disable_progress_bar

--- moncenterlib/gnss/different_tools.py
+++ moncenterlib/gnss/different_tools.py
@@ -28,3 +28,13 @@ def files_check(files: list) -> dict:
             output_check['error'].append(file)
 
     return output_check
+
+
+def disable_progress_bar(inc_bar):
+    def nothing():
+        pass
+    inc_bar.start = nothing
+    inc_bar.next = nothing
+    inc_bar.finish = nothing
+
+    return inc_bar
