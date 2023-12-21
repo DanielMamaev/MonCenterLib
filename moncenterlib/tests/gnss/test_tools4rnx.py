aca5ec9003c729bcd1ccdaed821561ade4d7d6de
DanielMamaev <mamaev.s.d@gmail.com>
2023-12-21 17:33:26 +0700

    add new test

--- moncenterlib/tests/gnss/test_tools4rnx.py
+++ moncenterlib/tests/gnss/test_tools4rnx.py
@@ -1,5 +1,6 @@
 import os
-from unittest import TestCase, main
+import subprocess
+from unittest import TestCase
 from unittest.mock import MagicMock, patch
 from pathlib import Path
 from moncenterlib.gnss.tools4rnx import RtkLibConvbin
@@ -8,8 +9,6 @@ from moncenterlib.gnss.tools4rnx import RtkLibConvbin
 class TestTools4Rnx(TestCase):
     def setUp(self) -> None:
         self.t4r = RtkLibConvbin()
-        self.path_data_raw = str(Path(__file__).resolve().parent)
-        self.path_data_output = str(Path(__file__).resolve().parent)
 
     def test_different_id_default_config(self):
         config = self.t4r.get_default_config()
@@ -29,28 +28,31 @@ class TestTools4Rnx(TestCase):
             self.t4r.scan_dir('test', False)
         self.assertEqual(str(ms.exception), "Path to dir is strange.")
 
-    # def test_func_scan_dir(self):
-    #     res = self.t4r.scan_dir(self.path_data_raw, False)
-    #     self.assertEqual(len(res), 2)
+    def test_func_scan_dir(self):
+        with patch("moncenterlib.gnss.tools4rnx.os.walk") as mock_walk:
+            mock_walk.return_value = (("/home/", None, ["file1", "file2"]), ("/home/2/", None, ["file3", "file4"]))
+            res = self.t4r.scan_dir("/", True)
+            self.assertEqual(len(res), 4)
+            self.assertEqual(['/home/file1', '/home/file2', '/home/2/file3', '/home/2/file4'], res)
 
-    #     res = self.t4r.scan_dir(self.path_data_raw, True)
-    #     self.assertEqual(len(res), 4)
+            mock_walk.return_value = (("/home/", None, []), ("/home/2/", None, ["file3", "file4"]))
+            res = self.t4r.scan_dir("/", True)
+            self.assertEqual(len(res), 2)
+            self.assertEqual(['/home/2/file3', '/home/2/file4'], res)
 
-    #     res = self.t4r.scan_dir(self.path_data_raw, False)
-    #     exp_res = [f"{self.path_data_raw}07.30.2020_17-00-00.ubx",
-    #                f"{self.path_data_raw}07.24.2020_17-00-00.ubx"]
-    #     res.sort()
-    #     exp_res.sort()
-    #     self.assertEqual(res, exp_res)
+            mock_walk.return_value = (("/home/", None, []), ("/home/2/", None, []))
+            res = self.t4r.scan_dir("/", True)
+            self.assertEqual(len(res), 0)
+            self.assertEqual([], res)
 
-    #     res = self.t4r.scan_dir(self.path_data_raw, True)
-    #     exp_res = [f"{self.path_data_raw}07.30.2020_17-00-00.ubx",
-    #                f"{self.path_data_raw}07.24.2020_17-00-00.ubx",
-    #                f"{self.path_data_raw}1/06.25.2020_17-00-18.ubx",
-    #                f"{self.path_data_raw}1/2/06.22.2020_06-24-18.ubx"]
-    #     res.sort()
-    #     exp_res.sort()
-    #     self.assertEqual(res, exp_res)
+        with (patch("moncenterlib.gnss.tools4rnx.os.listdir") as mock_listdir,
+              patch("moncenterlib.gnss.tools4rnx.os.path.isfile") as mock_isfile):
+            mock_isfile.side_effect = [True, True, False, False, True]
+            mock_listdir.return_value = ['/home/file1', '/home/file2', '/home/2/', '/home/2/', '/home/file3']
+
+            res = self.t4r.scan_dir("/", False)
+            self.assertEqual(len(res), 3)
+            self.assertEqual(['/home/file1', '/home/file2', '/home/file3'], res)
 
     def test_start_raises(self):
         with self.assertRaises(Exception) as ms:
@@ -80,50 +82,44 @@ class TestTools4Rnx(TestCase):
             self.t4r.start(os.curdir, 'path', self.t4r.get_default_config(), False, False)
         self.assertEqual(str(ms.exception), "Path to output dir is strange.")
 
-    # @patch('moncenterlib.gnss.tools4rnx.subprocess')
-    # def test_start_input_list(self, mock_subprocess):
-    #     mock_subprocess.run = MagicMock()
-    #     exp_list = [f"{self.path_data_raw}07.30.2020_17-00-00.ubx",
-    #                 f"{self.path_data_raw}07.24.2020_17-00-00.ubx"]
+    @patch('moncenterlib.gnss.tools4rnx.subprocess')
+    def test_start_with_input_list(self, mock_subprocess):
+        mock_subprocess.run = MagicMock()
 
-    #     list_files = self.t4r.scan_dir(self.path_data_raw)
-    #     self.t4r.start(list_files, self.path_data_output, self.t4r.get_default_config(), False, False)
-    #     exis_list = []
-    #     for i in mock_subprocess.run.call_args_list:
-    #         exis_list += i[0][0]
-    #     for i in exp_list:
-    #         self.assertIn(i, exis_list)
+        list_files = ['/home/file1', '/home/file2', '/home/2/file3', '/home/2/file4']
+        self.t4r.start(list_files, "/", self.t4r.get_default_config(), False, False)
 
-    #     mock_subprocess.run.call_args_list = []
-    #     list_files = exp_list
-    #     self.t4r.start(list_files, self.path_data_output, self.t4r.get_default_config(), False, False)
-    #     exis_list = []
-    #     for i in mock_subprocess.run.call_args_list:
-    #         exis_list += i[0][0]
-    #     for i in exp_list:
-    #         self.assertIn(i, exis_list)
+        exis_list = []
+        for i in mock_subprocess.run.call_args_list:
+            exis_list += i[0][0]
+        for i in list_files:
+            self.assertIn(i, exis_list)
 
-    # @patch('moncenterlib.gnss.tools4rnx.subprocess')
-    # def test_start_input_dir(self, mock_subprocess):
-    #     mock_subprocess.run = MagicMock()
-    #     exp_list = [f"{self.path_data_raw}07.30.2020_17-00-00.ubx",
-    #                 f"{self.path_data_raw}07.24.2020_17-00-00.ubx"]
+    def test_start_with_input_dir(self):
+        with (patch("moncenterlib.gnss.tools4rnx.subprocess") as mock_subprocess,
+              patch("moncenterlib.gnss.tools4rnx.RtkLibConvbin.scan_dir") as mock_scan_dir):
 
-    #     self.t4r.start(self.path_data_raw, self.path_data_output, self.t4r.get_default_config(), False, False)
-    #     exis_list = []
-    #     for i in mock_subprocess.run.call_args_list:
-    #         exis_list += i[0][0]
-    #     for i in exp_list:
-    #         self.assertIn(i, exis_list)
+            list_files = ['/home/file1', '/home/file2', '/home/2/file3', '/home/2/file4']
 
-    # @patch('moncenterlib.gnss.tools4rnx.subprocess')
-    # def test_start_input_file(self, mock_subprocess):
-    #     mock_subprocess.run = MagicMock()
-    #     exp_file = f"{self.path_data_raw}07.30.2020_17-00-00.ubx"
+            mock_subprocess.run = MagicMock()
+            mock_scan_dir.return_value = list_files
 
-    #     self.t4r.start(exp_file, self.path_data_output, self.t4r.get_default_config(), False, False)
-    #     exis_list = mock_subprocess.run.call_args_list[0][0][0]
-    #     self.assertIn(exp_file, exis_list)
+            self.t4r.start("/", "/", self.t4r.get_default_config(), False, False)
+            exis_list = []
+            for i in mock_subprocess.run.call_args_list:
+                exis_list += i[0][0]
+            for i in list_files:
+                self.assertIn(i, exis_list)
+
+    def test_start_with_input_file(self):
+        with (patch("moncenterlib.gnss.tools4rnx.subprocess") as mock_subprocess,
+              patch("moncenterlib.gnss.tools4rnx.os.path.isfile") as mock_isfile):
+            mock_isfile.return_value = True
+            mock_subprocess.run = MagicMock()
+
+            self.t4r.start('/home/file1', "/", self.t4r.get_default_config(), False, False)
+            exis_list = mock_subprocess.run.call_args_list[0][0][0]
+            self.assertIn('/home/file1', exis_list)
 
     @patch('moncenterlib.gnss.tools4rnx.subprocess')
     def test_raises_config(self, mock_subprocess):
@@ -290,156 +286,114 @@ class TestTools4Rnx(TestCase):
         self.assertEqual(str(ms.exception), "Config. Key: other_halfc. Unknown value 'nothing'.")
         config['other_halfc'] = '1'
 
-    # @patch('moncenterlib.gnss.tools4rnx.subprocess')
-    # def test_config(self, mock_subprocess):
-    #     mock_subprocess.run = MagicMock()
+    def test_config(self):
+        with (patch("moncenterlib.gnss.tools4rnx.subprocess.run") as mock_subprocess,
+              patch("moncenterlib.gnss.tools4rnx.os.path.isfile") as mock_isfile):
 
-    #     config = self.t4r.get_default_config()
-    #     config['format'] = 'ubx'
-    #     config['rinex_v'] = '3.03'
-    #     config['start_time'] = '2023/10/27 00:00:00'
-    #     config['end_time'] = '2023/12/27 00:00:00'
-    #     config['interval'] = '5'
-    #     config['freq'] = '1'
-    #     config['system'] = 'G,R'
-    #     config['output_o'] = '1'
-    #     config['output_n'] = '1'
-    #     config['output_g'] = '1'
-    #     config['output_h'] = '1'
-    #     config['output_q'] = '1'
-    #     config['output_l'] = '1'
-    #     config['output_b'] = '1'
-    #     config['output_i'] = '1'
-    #     config['output_s'] = '1'
-    #     config['other_od'] = '1'
-    #     config['other_os'] = '1'
-    #     config['other_oi'] = '1'
-    #     config['other_ot'] = '1'
-    #     config['other_ol'] = '1'
-    #     config['other_halfc'] = '1'
-    #     config['comment'] = 'comment'
-    #     config['marker_name'] = 'marker_name'
-    #     config['marker_number'] = 'marker_number'
-    #     config['marker_type'] = 'marker_type'
-    #     config['about_name'] = 'about_name'
-    #     config['about_agency'] = 'about_agency'
-    #     config['receiver_number'] = 'receiver_number'
-    #     config['receiver_type'] = 'receiver_type'
-    #     config['receiver_version'] = 'receiver_version'
-    #     config['antenna_number'] = 'antenna_number'
-    #     config['antenna_type'] = 'antenna_type'
-    #     config['approx_position_x'] = '1'
-    #     config['approx_position_y'] = '2'
-    #     config['approx_position_z'] = '3'
-    #     config['antenna_delta_h'] = '4'
-    #     config['antenna_delta_e'] = '5'
-    #     config['antenna_delta_n'] = '6'
+            mock_isfile.return_value = True
 
-    #     self.t4r.start(self.path_data_raw, self.path_data_output, config, False, False)
+            config = self.t4r.get_default_config()
+            config['format'] = 'ubx'
+            config['rinex_v'] = '3.03'
+            config['start_time'] = '2023/10/27 00:00:00'
+            config['end_time'] = '2023/12/27 00:00:00'
+            config['interval'] = '5'
+            config['freq'] = '1'
+            config['system'] = 'G,R'
+            config['output_o'] = '1'
+            config['output_n'] = '1'
+            config['output_g'] = '1'
+            config['output_h'] = '1'
+            config['output_q'] = '1'
+            config['output_l'] = '1'
+            config['output_b'] = '1'
+            config['output_i'] = '1'
+            config['output_s'] = '1'
+            config['other_od'] = '1'
+            config['other_os'] = '1'
+            config['other_oi'] = '1'
+            config['other_ot'] = '1'
+            config['other_ol'] = '1'
+            config['other_halfc'] = '1'
+            config['comment'] = 'comment'
+            config['marker_name'] = 'marker_name'
+            config['marker_number'] = 'marker_number'
+            config['marker_type'] = 'marker_type'
+            config['about_name'] = 'about_name'
+            config['about_agency'] = 'about_agency'
+            config['receiver_number'] = 'receiver_number'
+            config['receiver_type'] = 'receiver_type'
+            config['receiver_version'] = 'receiver_version'
+            config['antenna_number'] = 'antenna_number'
+            config['antenna_type'] = 'antenna_type'
+            config['approx_position_x'] = '1'
+            config['approx_position_y'] = '2'
+            config['approx_position_z'] = '3'
+            config['antenna_delta_h'] = '4'
+            config['antenna_delta_e'] = '5'
+            config['antenna_delta_n'] = '6'
 
-    #     exp_cmd = [str(Path(__file__).resolve().parent.joinpath("bin", "convbin")),
-    #                '-r', 'ubx',
-    #                '-v', '3.03',
-    #                '-ts', '2023/10/27 00:00:00',
-    #                '-te', '2023/12/27 00:00:00',
-    #                '-ti', '5',
-    #                '-f', '1',
-    #                '-y', 'C',
-    #                '-y', 'E',
-    #                '-y', 'I',
-    #                '-y', 'J',
-    #                '-y', 'S',
-    #                '-o', f'{self.path_data_output}07.24.2020_17-00-00.o',
-    #                '-n', f'{self.path_data_output}07.24.2020_17-00-00.n',
-    #                '-g', f'{self.path_data_output}07.24.2020_17-00-00.g',
-    #                '-h', f'{self.path_data_output}07.24.2020_17-00-00.h',
-    #                '-q', f'{self.path_data_output}07.24.2020_17-00-00.q',
-    #                '-l', f'{self.path_data_output}07.24.2020_17-00-00.l',
-    #                '-b', f'{self.path_data_output}07.24.2020_17-00-00.b',
-    #                '-i', f'{self.path_data_output}07.24.2020_17-00-00.i',
-    #                '-s', f'{self.path_data_output}07.24.2020_17-00-00.s',
-    #                '-od',
-    #                '-os',
-    #                '-oi',
-    #                '-ot',
-    #                '-ol',
-    #                '-halfc',
-    #                '-hc', 'comment',
-    #                '-hm', 'marker_name',
-    #                '-hn', 'marker_number',
-    #                '-ht', 'marker_type',
-    #                '-ho', 'about_name/about_agency',
-    #                '-hr', 'receiver_number/receiver_type/receiver_version',
-    #                '-ha', 'antenna_number/antenna_type',
-    #                '-hp', '1/2/3',
-    #                '-hd', '4/5/6',
-    #                f'{self.path_data_raw}07.24.2020_17-00-00.ubx']
+            self.t4r.start('/home/file1', "/", config, False, False)
 
-    #     self.assertEqual(mock_subprocess.run.call_args_list[0][0][0], exp_cmd)
+            exp_cmd = [str(Path(__file__).resolve().parent.parent.parent.joinpath("gnss", "bin", "convbin")),
+                       '-r', 'ubx',
+                       '-v', '3.03',
+                       '-ts', '2023/10/27 00:00:00',
+                       '-te', '2023/12/27 00:00:00',
+                       '-ti', '5',
+                       '-f', '1',
+                       '-y', 'C',
+                       '-y', 'E',
+                       '-y', 'I',
+                       '-y', 'J',
+                       '-y', 'S',
+                       '-o', '/file1.o',
+                       '-n', '/file1.n',
+                       '-g', '/file1.g',
+                       '-h', '/file1.h',
+                       '-q', '/file1.q',
+                       '-l', '/file1.l',
+                       '-b', '/file1.b',
+                       '-i', '/file1.i',
+                       '-s', '/file1.s',
+                       '-od',
+                       '-os',
+                       '-oi',
+                       '-ot',
+                       '-ol',
+                       '-halfc',
+                       '-hc', 'comment',
+                       '-hm', 'marker_name',
+                       '-hn', 'marker_number',
+                       '-ht', 'marker_type',
+                       '-ho', 'about_name/about_agency',
+                       '-hr', 'receiver_number/receiver_type/receiver_version',
+                       '-ha', 'antenna_number/antenna_type',
+                       '-hp', '1/2/3',
+                       '-hd', '4/5/6',
+                       '/home/file1']
 
-    @patch('moncenterlib.gnss.tools4rnx.subprocess')
-    @patch('moncenterlib.gnss.tools4rnx.IncrementalBar')
-    def test_show_process(self, mock_incrementalbar, mock_subprocess):
-        self.t4r.start(self.path_data_raw, self.path_data_output, self.t4r.get_default_config(), False, True)
-        self.assertGreater(len(mock_incrementalbar.mock_calls), 1)
+            self.assertEqual(mock_subprocess.call_args_list[0][0][0], exp_cmd)
 
-        mock_incrementalbar.mock_calls = []
-        self.t4r.start(self.path_data_raw, self.path_data_output, self.t4r.get_default_config(), False, False)
-        self.assertEqual(len(mock_incrementalbar.mock_calls), 1)
+    def test_show_process(self):
+        with (patch('moncenterlib.gnss.tools4rnx.subprocess'),
+              patch('moncenterlib.gnss.tools4rnx.IncrementalBar') as mock_incrementalbar):
+            self.t4r.start("/", "/", self.t4r.get_default_config(), False, True)
+            self.assertGreater(len(mock_incrementalbar.mock_calls), 1)
 
-    # def test_output_result_done(self):
-    #     config = self.t4r.get_default_config()
-    #     config['format'] = 'ubx'
-    #     config['output_o'] = '1'
-    #     config['output_n'] = '0'
-    #     res = self.t4r.start(self.path_data_raw, self.path_data_output, config, False, False)
-    #     res['done'].sort()
+            mock_incrementalbar.mock_calls = []
+            self.t4r.start("/", "/", self.t4r.get_default_config(), False, False)
+            self.assertEqual(len(mock_incrementalbar.mock_calls), 1)
 
-    #     exp_dict = {"done": [f'{self.path_data_output}07.24.2020_17-00-00.o',
-    #                          f'{self.path_data_output}07.30.2020_17-00-00.o'],
-    #                 "error": []}
-    #     exp_dict['done'].sort()
-    #     self.assertEqual(res, exp_dict)
-    #     self.clean_output()
+    def test_chech_make_convbin(self):
+        path = str(Path(__file__).resolve().parent.parent.parent.joinpath("gnss", "bin", "convbin"))
+        res = subprocess.run([path, "-h"], stderr=subprocess.DEVNULL, check=False)
+        self.assertEqual(0, res.returncode)
 
-    #     config['output_n'] = '1'
-    #     res = self.t4r.start(self.path_data_raw, self.path_data_output, config, False, False)
-    #     res['done'].sort()
-
-    #     exp_dict = {"done": [f'{self.path_data_output}07.24.2020_17-00-00.o',
-    #                          f'{self.path_data_output}07.30.2020_17-00-00.o',
-    #                          f'{self.path_data_output}07.24.2020_17-00-00.n',
-    #                          f'{self.path_data_output}07.30.2020_17-00-00.n',],
-    #                 "error": []}
-    #     exp_dict['done'].sort()
-    #     self.assertEqual(res, exp_dict)
-    #     self.clean_output()
-
-    # def test_output_result_error(self):
-    #     config = self.t4r.get_default_config()
-    #     config['format'] = 'ubx'
-    #     config['output_o'] = '0'
-    #     config['output_n'] = '0'
-    #     config['output_i'] = '1'
-    #     res = self.t4r.start(self.path_data_raw, self.path_data_output, config, False, False)
-    #     res['error'].sort()
-
-    #     exp_dict = {"error": [f'{self.path_data_output}07.24.2020_17-00-00.i',
-    #                           f'{self.path_data_output}07.30.2020_17-00-00.i'],
-    #                 "done": []}
-    #     exp_dict['error'].sort()
-    #     self.assertEqual(res, exp_dict)
-    #     self.clean_output()
-
-    #     config['output_g'] = '1'
-    #     res = self.t4r.start(self.path_data_raw, self.path_data_output, config, False, False)
-    #     res['error'].sort()
-
-    #     exp_dict = {"error": [f'{self.path_data_output}07.24.2020_17-00-00.i',
-    #                           f'{self.path_data_output}07.30.2020_17-00-00.i',
-    #                           f'{self.path_data_output}07.24.2020_17-00-00.g',
-    #                           f'{self.path_data_output}07.30.2020_17-00-00.g'],
-    #                 "done": []}
-    #     exp_dict['error'].sort()
-    #     self.assertEqual(res, exp_dict)
-    #     self.clean_output()
+    def test_output_result(self):
+        with (patch('moncenterlib.gnss.tools4rnx.subprocess')):
+            list_files = ['/home/file1', '/home/file2']
+            res = self.t4r.start(list_files, "/", self.t4r.get_default_config(), False, False)
+            self.assertEqual({"done": [],
+                              "error": ['/file1.o', '/file1.n',
+                                        '/file2.o', '/file2.n', ]}, res)
