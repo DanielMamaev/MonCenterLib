542f1e07a8db2a02174fe3575516da1962f0c034
DanielMamaev <mamaev.s.d@gmail.com>
2024-05-05 13:24:48 +0700

    add docstring

--- moncenterlib/gnss/gnss_time_series.py
+++ moncenterlib/gnss/gnss_time_series.py
@@ -4,6 +4,17 @@ from typeguard import typechecked
 
 @typechecked
 def parse_pos_file(path2file: str, sep: str | None = None) -> tuple[dict[str, list], list[list[str]]]:
+    """This function for parsing .pos file. The method returns header, name of columns and time serie.
+
+    Args:
+        path2file (str): Path to the file .pos
+        sep (str | None, optional): If .pos file has separation (e.g. ;) use sep=";". Defaults to None.
+
+    Returns:
+        tuple[dict[str, list], list[list[str]]]: Return tuple. First item is header of .pos file.
+            Second item is time serie.
+    """
+
     header = defaultdict(list)
     data = list()
 
