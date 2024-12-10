d717bc2fda711b433102b4e7b9fb955320c7b195
DanielMamaev <mamaev.s.d@gmail.com>
2024-12-10 23:29:24 +0700

    add type of value.

--- moncenterlib/gnss/gnss_time_series.py
+++ moncenterlib/gnss/gnss_time_series.py
@@ -46,6 +46,22 @@ def parse_pos_file(path2file: str, sep: str | None = None) -> tuple[dict[str, li
                     row[2] = ' '.join(row[2:5])  # date dms d m s
                     row.pop(3)  # date dms dms m
                     row.pop(3)  # date dms dms
+                else:
+                    row[1] = float(row[1]) # coord 1
+                    row[2] = float(row[2]) # coord 2
+                
+                row[3] = float(row[3]) # coord 3
+                row[4] = int(row[4]) # Q
+                row[5] = int(row[5]) # ns
+                
+                row[6] = int(row[6]) # sd coord 1
+                row[7] = int(row[7]) # sd coord 2
+                row[8] = int(row[8]) # sd coord 3
+                row[9] = int(row[9]) # sd coord 1 2
+                row[10] = int(row[10]) # sd coord 2 3
+                row[11] = int(row[11]) # sd coord 1 3
+                row[12] = int(row[12]) # age
+                row[13] = int(row[13]) # ratio 
 
                 data.append(row)
 
