6ba4ecc9ba2c659d7765c941b0bb616d8b60ae53
DanielMamaev <mamaev.s.d@gmail.com>
2025-02-11 19:35:46 +0700

    update docstring

--- moncenterlib/gnss/tools4rnx.py
+++ moncenterlib/gnss/tools4rnx.py
@@ -14,11 +14,12 @@ from collections import defaultdict
 import datetime
 from logging import Logger
 import os
+import queue
 import subprocess
 from pathlib import Path
 import threading
 from typeguard import typechecked
-from moncenterlib.tools import files_check, create_simple_logger, get_files_from_dir, get_path2bin
+from moncenterlib.tools import create_simple_logger, get_files_from_dir, get_path2bin
 
 
 class RtkLibConvbin:
@@ -32,9 +33,11 @@ class RtkLibConvbin:
     See code usage examples in the examples folder.
     """
     @typechecked
-    def __init__(self, logger: bool | Logger | None = None):
+    def __init__(self, workers: int = 1, logger: bool | Logger | None = None):
         """
         Args:
+            workers (int): The number of parallel processes. It reduces the processing time for multiple files.
+                Defaults to 1.
             logger (bool | Logger, optional): if the logger is None, a logger will be created inside the default class.
                 If the logger is False, then no information will be output.
                 If you pass an instance of your logger, the information output will be implemented according to your logger.
@@ -86,46 +89,21 @@ class RtkLibConvbin:
 
         if self.logger in [None, False]:
             self.logger = create_simple_logger("RtkLibConvbin", logger)
-        
-        self.__process = None
-        self.stdout_log = []
-        self.stderr_log = []
 
-    def get_default_config(self) -> dict:
-        """
-        Return variable __default_config. Default config isn't editable.
-        In the future, you will manually configure this config and send it to the start method.
-        See documentation RTKLIB, how to configuration.
+        self.workers = workers
+        self.semaphore = threading.Semaphore(self.workers)
 
-        Returns:
-            dict: default config for convbin of RTKLib
-        """
-        return self.__default_config.copy()
+        self.__create_vars()
 
-    @typechecked
-    def scan_dir(self, input_dir: str, recursion: bool = False) -> list[str]:
-        """This method scans the directory and makes a list of files for further work of the class.
-        The method can also recursively search for files.
+    def __create_vars(self):
+        self.__process = defaultdict(None)
 
-        Args:
-            input_dir (str): Path to the directory.
-            recursion (bool, optional): Recursively search for files. Defaults to False.
-
-        Raises:
-            ValueError: Path to dir is strange.
-
-        Returns:
-            list[str]: List of files.
-
-        Examples:
-            >>> t4r = RtkLibConvib()
-            >>> res = t4r.scan_dir("/some_path_to_dir", True)
-            >>> res
-            ["file_1", "file_2"]
-        """
-        input_files = get_files_from_dir(input_dir, recursion)
-
-        return input_files
+        def def_dict(): return {
+            'stdout': [],
+            'stderr': []
+        }
+        self.std_log = defaultdict(def_dict)
+        self.output_files = defaultdict(list)
 
     def __check_config(self, config: dict):
         format_raw = ["rtcm2", "rtcm3", "nov", "oem3", "ubx", "ss2",
@@ -202,10 +180,179 @@ class RtkLibConvbin:
                 self.logger.error("Config. Key: {f'other_%s'}. Unknown value '%s'.", t, config[f'other_{t}'])
                 raise ValueError(f"Config. Key: {f'other_{t}'}. Unknown value '{config[f'other_{t}']}'.")
 
+    def __make_config4convbin(self, filename: str, output_dir: str, config: dict, output_filename=""):
+        output_files = dict()
+        cmd = []
+
+        cmd += [get_path2bin("convbin")]
+
+        cmd += ["-r", config['format']]
+        cmd += ["-v", config['rinex_v']]
+
+        if not config['start_time'] == '':
+            cmd += ["-ts", config['start_time']]
+
+        if not config['end_time'] == '':
+            cmd += ["-te", config['end_time']]
+
+        cmd += ["-ti", config['interval']]
+        cmd += ["-f", config['freq']]
+
+        full_sys = {'G', 'R', 'E', 'J', 'S', 'C', 'I'}
+        sys = set(config['system'].split(','))
+        final_sys = list(full_sys - sys)
+        final_sys.sort()
+        for i in final_sys:
+            cmd += ["-y", i]
+
+        namef = os.path.basename(filename)
+        if Path(namef).suffix != '':
+            namef = namef.rsplit(Path(namef).suffix, 1)[0]
+
+        type_files = ['o', 'n', 'g', 'h', 'q', 'l', 'b', 'i', 's']
+        for t in type_files:
+            if config[f'output_{t}'] == '1':
+                if output_filename == "":
+                    temp_path = os.path.join(output_dir, f"{namef}.{t}")
+                    output_files[t] = os.path.join(output_dir, f"{namef}.{t}")
+                else:
+                    temp_path = os.path.join(output_dir, output_filename + f".{t}")
+                    output_files[t] = os.path.join(output_dir, output_filename + f"{namef.replace('*', '')}.{t}")
+                cmd += [f"-{t}", temp_path]
+
+        other_type = ['od', 'os', 'oi', 'ot', 'ol', 'halfc']
+        for t in other_type:
+            if config[f'other_{t}'] == '1':
+                cmd += [f"-{t}"]
+
+        cmd += ["-hc", config['comment']]
+
+        cmd += ["-hm", config['marker_name']]
+        cmd += ["-hn", config['marker_number']]
+        cmd += ["-ht", config['marker_type']]
+
+        cmd += ["-ho", f"{config['about_name']}/{config['about_agency']}"]
+        cmd += ["-hr", f"{config['receiver_number']}/{config['receiver_type']}/{config['receiver_version']}"]
+        cmd += ["-ha", f"{config['antenna_number']}/{config['antenna_type']}"]
+        cmd += ["-hp", f"{config['approx_position_x']}/{config['approx_position_y']}/{config['approx_position_z']}"]
+        cmd += ["-hd", f"{config['antenna_delta_h']}/{config['antenna_delta_e']}/{config['antenna_delta_n']}"]
+
+        cmd += [filename]
+
+        return cmd, output_files
+
+    def __start_converting(self, cmd: list, wait_process=False):
+        filename = cmd[-1]
+        self.__process[filename] = subprocess.Popen(
+            cmd,
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            text=True,
+            bufsize=1,
+            universal_newlines=True
+        )
+
+        def read_output(stream, filename):
+            for line in stream:
+                self.std_log[filename]['stdout'] += [line.strip()]
+
+        def read_error(stream, filename):
+            for line in stream:
+                self.std_log[filename]['stderr'] += [line.strip()]
+
+        stdout_thread = threading.Thread(target=read_output, args=(self.__process[filename].stdout, filename))
+        stderr_thread = threading.Thread(target=read_error, args=(self.__process[filename].stderr, filename))
+
+        stdout_thread.start()
+        stderr_thread.start()
+
+        if wait_process:
+            self.__process[filename].wait()
+
+        # self.logger.info("Finish converting file %s", cmd[-1])
+
+    def get_default_config(self) -> dict:
+        """
+        Return variable __default_config. Default config isn't editable.
+        In the future, you will manually configure this config and send it to the start method.
+        See documentation RTKLIB, how to configuration.
+
+        Returns:
+            dict: default config for convbin of RTKLib
+        """
+        return self.__default_config.copy()
+
     @typechecked
-    def start(self, input_raw: str | list, output: str, config: dict,
-              recursion: bool = False) -> dict:
-        """The method starts the process of preserving files in the RINEX format.
+    def scan_dir(self, input_dir: str, recursion: bool = False) -> list[str]:
+        """This method scans the directory and makes a list of files for further work of the class.
+        The method can also recursively search for files.
+
+        Args:
+            input_dir (str): Path to the directory.
+            recursion (bool, optional): Recursively search for files. Defaults to False.
+
+        Raises:
+            ValueError: Path to dir is strange.
+
+        Returns:
+            list[str]: List of files.
+
+        Examples:
+            >>> t4r = RtkLibConvib()
+            >>> res = t4r.scan_dir("/some_path_to_dir", True)
+            >>> res
+            ["file_1", "file_2"]
+        """
+        input_files = get_files_from_dir(input_dir, recursion)
+
+        return input_files
+
+    @typechecked
+    def merge_rinex(self,
+                    input_dir: str,
+                    output_dir: str,
+                    output_filename: str,
+                    config: dict,
+                    wait_process: bool = True):
+        """Allows you to combine several RINEX files into one common file.
+
+        Args:
+            input_dir (str): Path to the directory. There should definitely be an asterisk at the end of the path.
+                For example /input_dir/*
+            output_dir (str): Path to the output directory.
+            output_filename (str): Name of the output file.
+            config (dict): Dictionary with configuration.
+                You can get the configuration by calling the get_default_config() method.
+            wait_process (bool): If wait_process is set to False, the file merging process is performed in a separate thread.
+                The method becomes non-blocking. If wait_process is True, then the process becomes blocking.
+                The file merging process is waiting to be completed.
+
+        Examples:
+            >>> t4r = RtkLibConvib()
+            >>> config = t4r.get_default_config()
+            >>> t4r.merge_rinex("/input_dir/*", "/output_dir", "name_file", config, True)
+            >>> print(t4r.output_files)
+        """
+
+        self.logger.info("Start merging files")
+
+        self.__check_config(config)
+        self.__create_vars()
+
+        cmd, output_files = self.__make_config4convbin(input_dir, output_dir, config, output_filename)
+        for type_file, filename in output_files.items():
+            self.output_files[type_file].append(filename)
+
+        self.__start_converting(cmd, wait_process)
+
+    @typechecked
+    def start(self,
+              input_raw: str | list,
+              output: str,
+              config: dict,
+              recursion: bool = False):
+        """The method starts the process of preserving files in the RINEX format.This method is non-blocking.
+        You can use an endless loop to wait for the process to complete. See the usage example.
 
         Args:
             input_raw (str | list): The input method can accept a path of up to one file.
@@ -225,24 +372,28 @@ class RtkLibConvbin:
             TypeError: The type of the 'input_raw' variable should be 'str' or 'list'.
             ValueError: Path to output dir is strange.
 
-        Returns:
-            dict: A dictionary contains list of output files. A key is type of file.
-            A value contains a dictionary. The dictionary contains two keys. Done and no_exists.
-            The done key stores a list of files that have been successfully created.
-            The no_exists key stores a list of files that have not been created.
-
         Examples:
             >>> t4r = RtkLibConvib()
             >>> list_files = t4r.scan_dir("/some_path_to_dir", True)
             >>> config = t4r.get_default_config()
-            >>> config["format"] = "ubx"
-            >>> res = t4r.start(list_files, "/some_output_dir", config, True)
-            >>> res
-            {
-                'o': {"done": ["file_1"], "no_exists": ["file_2"]},
-                'n': {"done": [], "no_exists": ["file_3"]}
-            }
+            >>> t4r.start(list_files, "/some_output_dir", config, True)
+            >>> while True:
+            ...     time.sleep(1)
+            ...     status = t4r.get_last_status()
+            ...     print(status)
+            ...     states = [s['isStop'] for s in status.values()]
+            ...     if all(states):
+            ...         break
+            >>> print(t4r.output_files)
+            >>> # check output files
+            >>> from moncenterlib.tools import files_check
+            >>> return_files = {}
+            >>> for type_file, files in t4r.output_files.items():
+            ...     return_files[type_file] = files_check(files)
+            >>> print(return_files)
+
         """
+        self.logger.info("Start converting")
         self.logger.info("Checking config")
         self.__check_config(config)
 
@@ -262,99 +413,35 @@ class RtkLibConvbin:
             self.logger.error("Path to output dir is strange.")
             raise ValueError("Path to output dir is strange.")
 
-        # запуск конвертации
-        output_files = defaultdict(list)
+        self.__create_vars()
+
+        q = queue.Queue()
 
         for file in input_files:
-            self.logger.info("Start converting %s", file)
-            cmd = []
+            self.logger.info("Set config for %s", file)
+            cmd, output_files = self.__make_config4convbin(file, output, config)
 
-            cmd += [get_path2bin("convbin")]
+            for type_file, filename in output_files.items():
+                self.output_files[type_file].append(filename)
 
-            cmd += ["-r", config['format']]
-            cmd += ["-v", config['rinex_v']]
+            q.put(cmd)
 
-            if not config['start_time'] == '':
-                cmd += ["-ts", config['start_time']]
+        def worker(q: queue.Queue):
+            while not q.empty():
+                cmd = q.get()
 
-            if not config['end_time'] == '':
-                cmd += ["-te", config['end_time']]
+                with self.semaphore:
+                    self.logger.info("Start converting file %s", cmd[-1])
+                    self.__start_converting(cmd, True)
 
-            cmd += ["-ti", config['interval']]
-            cmd += ["-f", config['freq']]
+                q.task_done()
 
-            full_sys = {'G', 'R', 'E', 'J', 'S', 'C', 'I'}
-            sys = set(config['system'].split(','))
-            final_sys = list(full_sys - sys)
-            final_sys.sort()
-            for i in final_sys:
-                cmd += ["-y", i]
+        for _ in range(self.workers):
+            thread = threading.Thread(target=worker, args=(q,))
+            thread.start()
 
-            namef = os.path.basename(file)
-            if Path(namef).suffix != '':
-                namef = namef.rsplit(Path(namef).suffix, 1)[0]
-
-            type_files = ['o', 'n', 'g', 'h', 'q', 'l', 'b', 'i', 's']
-            for t in type_files:
-                if config[f'output_{t}'] == '1':
-                    temp_path = os.path.join(output, f"{namef}.{t}")
-                    cmd += [f"-{t}", temp_path]
-                    output_files[t].append(os.path.join(output, f"{namef}.{t}"))
-
-            other_type = ['od', 'os', 'oi', 'ot', 'ol', 'halfc']
-            for t in other_type:
-                if config[f'other_{t}'] == '1':
-                    cmd += [f"-{t}"]
-
-            cmd += ["-hc", config['comment']]
-
-            cmd += ["-hm", config['marker_name']]
-            cmd += ["-hn", config['marker_number']]
-            cmd += ["-ht", config['marker_type']]
-
-            cmd += ["-ho", f"{config['about_name']}/{config['about_agency']}"]
-            cmd += ["-hr", f"{config['receiver_number']}/{config['receiver_type']}/{config['receiver_version']}"]
-            cmd += ["-ha", f"{config['antenna_number']}/{config['antenna_type']}"]
-            cmd += ["-hp", f"{config['approx_position_x']}/{config['approx_position_y']}/{config['approx_position_z']}"]
-            cmd += ["-hd", f"{config['antenna_delta_h']}/{config['antenna_delta_e']}/{config['antenna_delta_n']}"]
-
-            cmd += [file]
-
-            self.stdout_log = []
-            self.stderr_log = []
-
-            self.__process = subprocess.Popen(
-                cmd,
-                stderr=subprocess.PIPE,  
-                stdout=subprocess.PIPE,  
-                text=True,               
-                bufsize=1,               
-                universal_newlines=True
-            )
-
-            def read_output(stream):
-                for line in stream:
-                    self.stdout_log += [line.strip()]
-
-            def read_error(stream):
-                for line in stream:
-                    self.stderr_log += [line.strip()]
-
-            stdout_thread = threading.Thread(target=read_output, args=(self.__process.stdout,))
-            stderr_thread = threading.Thread(target=read_error, args=(self.__process.stderr,))
-
-            stdout_thread.start()
-            stderr_thread.start()
-
-            
-        return_files = {}
-        for type_file, files in output_files.items():
-            return_files[type_file] = files_check(files)
-
-        return return_files
-    
     @typechecked
-    def get_last_status(self) -> dict[str, str | bool]:
+    def get_last_status(self) -> dict[str, dict]:
         """Returns the last line of processing status information.
 
         Returns:
@@ -362,7 +449,7 @@ class RtkLibConvbin:
             The stderr key stores the last line of error information.
             The stdout key stores the last line of information.
             The isStop key stores a boolean value. True if the process has stopped, False otherwise.
-        
+
         Examples:
             >>> t4r = RtkLibConvib()
             >>> t4r.start(list_files, "/some_output_dir", config, True)
@@ -373,30 +460,35 @@ class RtkLibConvbin:
             >>>     time.sleep(1)
             >>>     print(status)
             {
-                "isStop": False,
+                "isStop": True,
                 "stdout": "some info",
                 "stderr": ""
             }
-        
-        """
-        isStop = False 
-        
-        if self.__process is not None:
-            if self.__process.poll() is not None:
-                isStop = True
-        
-        output_status = {
-            "isStop": isStop,
-            "stdout": "",
-            "stderr": ""
-        }
 
-        if self.stderr_log != []:
-            output_status["stderr"] = self.stderr_log[-1]
-        if self.stdout_log != []:
-            output_status["stdout"] = self.stdout_log[-1]
+        """
+
+        output_status = defaultdict(dict)
+
+        for file, std in self.std_log.items():
+            stdout = ''
+            stderr = ''
+
+            isStop = False
+            if self.__process[file] is not None and self.__process[file].poll() is not None:
+                isStop = True
+
+            if std["stdout"] != []:
+                stdout = std["stdout"][-1]
+            if std["stderr"] != []:
+                stderr = std["stderr"][-1]
+
+            output_status[file] = {"stdout": stdout,
+                                   "stderr": stderr,
+                                   "isStop": isStop
+                                   }
+
         return output_status
-    
+
     @typechecked
     def get_full_status(self) -> dict[str, list[str]]:
         """Returns the full lines of processing status information.
@@ -405,7 +497,7 @@ class RtkLibConvbin:
             dict[str, list[str]]: The dictionary contains 2 keys. stderr, stdout.
             The stderr key stores the list lines of error information.
             The stdout key stores the list lines of information.
-        
+
         Examples:
             >>> t4r = RtkLibConvib()
             >>> t4r.start(list_files, "/some_output_dir", config, True)
@@ -414,9 +506,6 @@ class RtkLibConvbin:
                 "stdout": ["some info", "blabla"]
                 "stderr": []
             }
-            
+
         """
-        return {
-            "stdout": self.stdout_log,
-            "stderr": self.stderr_log
-        }
+        return self.std_log
